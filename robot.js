5e48422eb648af2184331a72b824fd9eaa182d0c
Vivek M <66559749+viv511@users.noreply.github.com>
2022-10-20 23:36:34 -0400

    coordinates

--- robot.js
+++ robot.js
@@ -1,4 +1,26 @@
+let resetYet = true;
+
 window.addEventListener('mousemove', function (e) {
     document.getElementById('x-value').textContent = e.x;
     document.getElementById('y-value').textContent = e.y;
 });
+
+window.addEventListener('mousedown', function(e) {
+    var rect = canvas.getBoundingClientRect();
+    var x = (e.clientX - rect.left); // x == the location of the click in the document - the location (relative to the left) of the canvas in the document
+    var y = (e.clientY - rect.top); // y == the location of the click in the document - the location (relative to the top) of the canvas in the document
+    var pointSize = 10;
+    var ctx = document.getElementById("canvas").getContext("2d");
+    ctx.fillStyle = "#00000"; // Red color
+
+    //draw dot
+    ctx.beginPath(); 
+    ctx.arc(x, y, pointSize, 0, Math.PI * 2, true); 
+    ctx.fill();
+});
+
+function resetCanvas() {
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    resetYet = true;
+}
