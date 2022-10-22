2534fb127339dd106d34bb0fef741741f45d2e45
Vivek M <66559749+viv511@users.noreply.github.com>
2022-10-22 18:02:17 -0400

    line between coords

--- robot.js
+++ robot.js
@@ -1,26 +1,40 @@
 // var xValues = [];
 // var yValues = [];
-var xValues = [];
-var yValues = [];
-
+let xValues = [];
+let yValues = [];
+let ctx;
 
 window.addEventListener('mousemove', function (e) {
     document.getElementById('x-value').textContent = e.x;
     document.getElementById('y-value').textContent = e.y;
+    ctx = document.getElementById("canvas").getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(xValues[0], yValues[0]);
+    for(let i=1; i<xValues[length]; i++) {
+        ctx.lineTo(xValues[i], yValues[i]);
+    }
+    ctx.stroke();
 });
 
 window.addEventListener('mousedown', function(e) {
     var rect = canvas.getBoundingClientRect();
     var x = (e.clientX - rect.left); // x == the location of the click in the document - the location (relative to the left) of the canvas in the document
     var y = (e.clientY - rect.top); // y == the location of the click in the document - the location (relative to the top) of the canvas in the document
-    var pointSize = 10;
-    var ctx = document.getElementById("canvas").getContext("2d");
+    var prevX = x;
+    var prevY = y;
+    var pointSize = 5;
+    // var canvasContext = document.getElementById("canvas").getContext("2d");
     ctx.fillStyle = "#00000"; // Red color
 
     //draw dot
-    ctx.beginPath(); 
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(x, y); 
+    ctx.stroke();
+    ctx.beginPath();
     ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
     ctx.fill();
+
     addPoint(x, y); 
 });
 
@@ -32,7 +46,18 @@ function addPoint(xVal, yVal) {
 }
 
 function resetCanvas() {
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    xValues = [];
+    yValues = [];
 }
+
+function drawLines() {
+    ctx.beginPath();
+    ctx.moveTo(120, 800);
+    ctx.lineTo(300, 600);
+    ctx.fill();
+}
+
+
+
 
